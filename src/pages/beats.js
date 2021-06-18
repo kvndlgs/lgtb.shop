d7dafa27068ee2e5968026bf74bfb83f2856efd1
kvndlgs <es2thekay@gmail.com>
2021-06-18 17:21:39 -0400

    switching to bs

--- src/pages/beats.js
+++ src/pages/beats.js
@@ -24,13 +24,7 @@ export default function Beats(){
     return (
         <>
     <PlayerWrapper>
-    <iframe 
-    id="mfs_html5" 
-    src="https://airbit.com/widgets/html5/?uid=468807&config=1090762" 
-    frameborder="0" 
-    scrolling="no"
-    title="shop" 
-    />
+    <iframe src="https://player.beatstars.com/?storeId=122232" width="100%" height="800" style="max-width:1024px;"> -- none -- </iframe>
     </PlayerWrapper>
 </>
     )
